6f52285daee586a6461c0c9123edb732f4748a42
dutra21 <mdutra11@gmail.com>
2024-12-14 20:20:50 -0400

    config

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,9 @@ module.exports = {
   collectCoverageFrom: [
     'src/**/*.ts(x)?',
     '!src/app/**', // should be tested in e2e
-    '!src/lib/registry.tsx'
+    '!src/lib/registry.tsx',
+    '!src/types/**',
+    '!src/styles/**'
   ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/'],
